69b4e671844a4c892f0ebabd3b1fe5b549d076ca
marcoaquintanar <marcoaquintanar@outlook.com>
2025-01-24 10:55:15 -0700

    Calculamos coordenadas enteras

--- linea/app.py
+++ linea/app.py
@@ -1,4 +1,6 @@
 #Cálculo de coordenadas de líneas
+from funciones import calcular_y
+
 def calcular_y(x, m, b):
 
     return m*x +b
@@ -6,8 +8,11 @@ def calcular_y(x, m, b):
 def main():
     m=2
     b=3
-    x=5
-    y= calcular_y(x, m, b)
+    X=[x for x in range(1,11)]
+    Y= [funciones.calcular_y(x,m,b) for x in X]
+    coordenadas_enteros = list(zip(X,Y))
+    print(coordenadas_enteros)
     print(f'Para x = {x}, y = {y}')
 
+if __name__ == '__main__':
     main()
