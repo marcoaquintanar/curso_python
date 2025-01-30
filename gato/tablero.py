ee86725071709a13bb31673cf44c4ec871917b45
marcoaquintanar <marcoaquintanar@outlook.com>
2025-01-30 10:46:26 -0700

    Agregué funcion usuario y ia

--- gato/tablero.py
+++ gato/tablero.py
@@ -17,11 +17,43 @@ def dibuja_tablero(simbolos:dict):
         {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
         ''' )
     
+def ia(simbolos:dict):
+    ''' Juega la maquina'''
+    ocupado = True
+    while ocupado == True:
+        x = random.choice(list(simbolos.keys()))
+        if simbolos[x] not in ['X','O']:
+            simbolos[x] = 'O'
+            ocupado = False
+    
+def usuario(simbolos:dict):
+    ''' Juega el usuario'''
+    ocupado = True
+    while ocupado == True:
+        numeros = [str(i) for i in range(1,10)]
+        x = input('Ingresa el el numero de la casilla: ')
+        if(x in numeros):
+            if simbolos[x] not in ['X','O']:
+                simbolos[x] = 'X'
+                ocupado = False
+        else:
+            print('Numero no valido')
+    
+import random    
 if __name__ == '__main__':
-    numeros = [str(x) for x in range(1,10)]
-    simbolos = {x:x for x in numeros}
-    dibuja_tablero(simbolos)
-    simbolos['1']='X'
-    dibuja_tablero(simbolos)
-    simbolos['5']='O'
-    dibuja_tablero(simbolos)
+
+    numeros = [str(i) for i in range(1, 10)]
+    dsimbolos = {x:x for x in numeros}
+    dibuja_tablero(dsimbolos)
+    ia(dsimbolos)
+    #x = random.choice(numeros)
+    #numeros.remove(x)
+    #dsimbolos[x] = 'X'
+    dibuja_tablero(dsimbolos)
+    usuario(dsimbolos)
+    dibuja_tablero(dsimbolos)
+    #o = random.choice(numeros)
+    #numeros.remove(o)
+    #simbolos[o] = 'O'
+    #dibuja_tablero(dsimbolos)
+    #print(numeros)
